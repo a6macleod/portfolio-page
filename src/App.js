231da940322e61f53e8cb83468df03de3f31183c
Andrew MacLeod <A6macleod@gmail.com>
2020-12-28 15:55:32 -0500

    Add three example projects and style them

--- src/App.js
+++ src/App.js
@@ -8,18 +8,46 @@ import Contact from "./components/Contact";
 import "./App.css";
 // Images for example projects
 import brickWall from "./images/brick-wall.png";
+import memory from "./images/memory.png";
+import responsive from "./images/responsove.png";
 
 function App() {
   const [exampleProjects, setExampleProjects] = useState([
     {
-      title: "Shopping Cart",
-      goal: "The goal of this site was to create a shopping cart using React",
-      type: "This was a learning project for React",
-      tech: "Javascript, React",
+      title: "React Shopping Cart Project",
+      goal:
+        "The goal of this site was some fun practice to create a shopping cart using React. Site includes a splash page, a shopping page, a checkout page, and a dynamically created page for each of the store items.",
+      tech:
+        "The tech used was mostly Javascript and React and the site is hosted on Github Pages.",
       link: "https://a6macleod.github.io/react_shopping_cart/",
+      code: "https://github.com/a6macleod/react_shopping_cart",
       img: brickWall,
       imgAlt:
         "Screenshot of fictional brick walls for sale from the Shopping Cart project",
+    },
+    {
+      title: "Memory Game",
+      goal:
+        "The goal of this project was to create a memory game using React. The images all change position each time one is clicked. Your score is tracked and if you click the same image twice, the score resets and your top score is recorded.",
+      tech:
+        "The tech used was mostly Javascript and React and the site is hosted on Github Pages.",
+      link: "https://a6macleod.github.io/memory-game/",
+      code: "https://github.com/a6macleod/memory-game",
+      img: memory,
+      imgAlt:
+        "Screenshot of the memory game the images are of various small metal gears.",
+    },
+    {
+      title: "Responsive Design",
+      goal:
+        "The goal of this project was to create a duplicate of TheNextWeb.com and replicate their responsive design. This was great practice making elements disapear, re-arrange, and introduce a hamburger menu. ",
+      tech:
+        "The tech used was just HTML and CSS. The site is hosted on Github Pages.",
+      link: "https://a6macleod.github.io/responsive-design/#",
+      code: "https://github.com/a6macleod/responsive-design",
+      img: responsive,
+      imgAlt:
+        "Screenshot of the responsive design project that duplicated The Next Webs website.",
     },
   ]);
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,29 @@
 const Projects = (props) => {
   return (
     <div className="projects">
-      <h2>Projects page section</h2>
       {props.exampleProjects.map((project) => {
         return (
-          <div className="project">
-            <img src={project.img} alt={project.imgAlt} />
-            <h2>{project.title}</h2>
-            <p>{project.goal}</p>
-            <p>{project.type}</p>
-            <p>{project.tech}</p>
-            <a href={project.link} target="_blank">
-              Check it out
-            </a>
+          <div key={project.title} className="project">
+            <div className="imgContainer">
+              <a href={project.link} target="_blank" rel="noreferrer">
+                <img src={project.img} alt={project.imgAlt} />
+              </a>
+            </div>
+            <div className="projectInfo">
+              <h2>{project.title}</h2>
+              <p>{project.goal}</p>
+              <p>{project.tech}</p>
+              <div className="links">
+                <a href={project.link} target="_blank" rel="noreferrer">
+                  Check out the site
+                  <i className="far fa-share-square"></i>
+                </a>
+                <a href={project.code} target="_blank" rel="noreferrer">
+                  See the code
+                  <i className="far fa-share-square"></i>
+                </a>
+              </div>
+            </div>
           </div>
         );
       })}
