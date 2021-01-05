381321ecb5f6b260157431c7050bfadfce5f6f4f
Andrew MacLeod <A6macleod@gmail.com>
2021-01-05 08:33:08 -0500

    Scale image sizes down, tweak contact form for netlify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,19 +3,22 @@ const Contact = () => {
     <div className="contact">
       <div className="formContainer">
         <form
-          name="contact"
+          name="contactForm"
           method="POST"
-          data-netlify="true"
           data-netlify-recaptcha="true"
+          netlify
         >
           <div className="formInput">
-            <label>Your Name:</label> <input type="text" name="name" />
+            <label for="name">Your Name:</label>
+            <input type="text" name="name" id="name" />
           </div>
           <div className="formInput">
-            <label>Your Email:</label> <input type="email" name="email" />
+            <label for="email">Your Email:</label>
+            <input type="email" name="email" id="email" />
           </div>
           <div className="formInput">
-            <label>Message:</label> <textarea name="message"></textarea>
+            <label for="message">Message:</label>
+            <textarea name="message" id="message"></textarea>
           </div>
           <div data-netlify-recaptcha="true"></div>
           <div className="formContactButtonContainer">
